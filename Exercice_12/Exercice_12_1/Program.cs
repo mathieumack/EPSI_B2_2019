47cc9ce16cc87c077ea6c117b3f4b3b2201822ee
MACK Mathieu <mathieu.mack@econocom.com>
2019-10-03 11:18:08 +0200

    Exercice 12

---
+++ Exercice_12/Exercice_12_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercice_12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableau = new int[10, 10];
+            Random random = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tableau[i, j] = random.Next(10);
+                }
+            }
+
+            // Affichage du tableau :
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tableau[i, j]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+
+            // Inversion
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i; j < 10; j++)
+                {
+                    int valueB = tableau[j, i];
+                    tableau[j, i] = tableau[i, j];
+                    tableau[i, j] = valueB;
+                }
+            }
+
+            // Affichage du tableau :
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tableau[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
---
+++ Exercice_9/Exercice_9_2/Ciseaux2Action.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice_9_2
+{
+    public class Ciseaux2Action : CiseauxAction
+    {
+        public override bool Gagne(Action action)
+        {
+            return false;
+        }
+    }
+}
